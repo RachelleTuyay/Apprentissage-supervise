8b956048a5fd9ac9bcda36c6ac98ee78959e5ac7
Rachelle <156919922+RachelleTuyay@users.noreply.github.com>
2025-11-27 14:53:11 +0100

    Update nettoyage.py

--- scripts/nettoyage.py
+++ scripts/nettoyage.py
@@ -4,7 +4,7 @@ import polars as pl
 fichier = "corpus_filtred.xlsx"
 df = pl.read_excel(fichier)
 
-# Nettoyer la colonne "question" en enlevant les balises #spk1: et #spk2:
+# Nettoyer la colonne "question" en enlevant les "#spk1:", "#spk2:", "#spk3:" et "#spk5:"
 df = df.with_columns(
     pl.col("question")
     .str.replace_all(r"#spk1:", "")  # supprime #spk1:
@@ -12,12 +12,6 @@ df = df.with_columns(
     .str.replace_all(r"#spk3:", "")  # supprime #spk3:
     .str.replace_all(r"#spk5:", "")  # supprime #spk5:
     .alias("question")
-)
-
-# Supprimer les disfluences du type : hm, euh, hésitations, répétitions, etc.
-df = df.with_columns(
-    pl.col("question")
-    .str.replace(r"\b(euh|hm+)\b", "", literal=False)  # euh, hm...
 )
 
 #Supprimer les espaces en trop autour du texte
